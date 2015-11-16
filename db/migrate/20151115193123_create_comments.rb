151b4a9d4b88129d4a52e69d3dbb17ce3d6916fc
kady <elkadyabdelrahman@gmail.com>
2015-11-16 20:57:38 +0200

    Removing the usage of add_reference in migrations as we are using in the same table

--- db/migrate/20151115193123_create_comments.rb
+++ db/migrate/20151115193123_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.string :body
-      add_reference :comment, :user, index: true, foreign_key: true
-      add_reference :comment, :post, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+      t.references :post, index: true, foreign_key: true
       t.timestamps null: false
     end
   end
--- db/migrate/20151115195113_create_likes.rb
+++ db/migrate/20151115195113_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration
   def change
     create_table :likes do |t|
-      add_reference :like, :user, index: true, foreign_key: true
-      add_reference :like, :post, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+      t.references :post, index: true, foreign_key: true
       t.timestamps null: false
     end
   end
