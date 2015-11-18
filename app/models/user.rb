5db50020797e8f1f24c28fed3ea795e1221ca39f
kady <elkadyabdelrahman@gmail.com>
2015-11-18 22:58:07 +0200

    Updating user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
   has_many :likes
   has_many :comments
-  has_many :posts
   has_many :goals
+
+  has_many :created_posts, :class_name => 'Post', :foreign_key => 'creator_id'
+  has_many :profile_posts, :class_name => 'Post', :foreign_key => 'profile_id'
+
 
   delegate :profile_posts, :goal_posts, :creator_posts, to: :posts
 end
