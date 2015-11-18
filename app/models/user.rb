986bd6f324d4a9ff37907ce7ee833defbc6b9edd
Abkreno <mohamedelhamamsy789@gmail.com>
2015-11-18 23:55:44 +0200

    Adds modeling and migrations for followings

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ActiveRecord::Base
   has_many :goals
 
   has_many :created_posts, :class_name => 'Post', :foreign_key => 'creator_id'
-  has_many :goal_posts, :class_name => 'GoalPost', :foreign_key => 'creator_id'
-  has_many :profile_posts, :class_name => 'ProfilePost', :foreign_key => 'profile_id'
+  has_many :profile_posts, :class_name => 'Post', :foreign_key => 'profile_id'
+
+  has_many :followers, :class_name => 'Following', :foreign_key => 'user_id'
+  has_many :followings, :class_name => 'Following', :foreign_key => 'follower_id'
 
   def posts
     Post.where("creator_id = ? or profile_id = ?",id,id)
