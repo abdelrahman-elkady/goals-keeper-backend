e5000bbb9d92bb97c365a625fd0c506ea7aadcad
kady <elkadyabdelrahman@gmail.com>
2015-11-19 01:11:31 +0200

    Fixing typos in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,14 @@ abkr = User.create(first_name: 'mohammed',last_name: 'mostafa',city: 'maadi',cou
 abdo = User.create(first_name: 'abdelrahman',last_name: 'mahmoud',city: 'haram',country: 'egypt',gender: true,date_of_birth: DateTime.now,profile_picture: '#')
 
 a_plus = Goal.create(title: 'I need to get A+ in all subjects',description: 'because i want to be a rank')
-quit_somking = Goal.create(title: 'I need to stop smoking',description: 'because ciggarittes are expensive')
+quit_smoking = Goal.create(title: 'I need to stop smoking',description: 'because ciggarittes are expensive')
 
 
 ali.goals << a_plus
 ali.save
 
 kady.goals << a_plus
-kady.goals << quit_somking
+kady.goals << quit_smoking
 kady.save
 
 post_1 = GoalPost.new
@@ -30,7 +30,7 @@ post_2.save
 post_3 = GoalPost.new
 post_3.text = 'i would like to improve my GPA this semester too'
 post_3.creator = abdo
-post_1.goal = quit_somking
+post_3.goal = quit_smoking
 post_3.save
 
 
