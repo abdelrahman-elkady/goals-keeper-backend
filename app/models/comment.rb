bfa464b9a14446ecd50ccddc149af66de1fb59e4
kady <elkadyabdelrahman@gmail.com>
2015-11-19 02:22:56 +0200

    Validating Comments and Likes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+
+  validates :body, :presence => true
+  validates :user, :presence => true
+  validates :post, :presence => true
+	validates_length_of :body, :minimum => 2, :allow_blank => false
+
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,7 @@
 class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+
+  validates :user, :presence => true
+  validates :post, :presence => true
 end
