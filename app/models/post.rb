e30b9efde80c9059287e8690a940fe19ef7369d8
Abdelrahman <a.mahmoud1994@gmail.com>
2015-12-14 21:40:57 +0200

    Removing the profile-post dependancy from the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
 
 	validates :creator, :presence => true
 	validates_length_of :text, :minimum => 5, :maximum => 50, :allow_blank => false
-	validates_inclusion_of :type, :in => ["GoalPost", "ProfilePost"]
+	validates_inclusion_of :type, :in => ["GoalPost"]
 
 
     has_many :comments
@@ -11,9 +11,8 @@ class Post < ActiveRecord::Base
     belongs_to :creator, :class_name => "User", foreign_key: "creator_id"
 
     scope :goal_posts, -> { where(type: 'GoalPost') }
-    scope :profile_posts, -> { where(type: 'ProfilePost') }
 
     def self.types
-      %w(GoalPost ProfilePost)
+      %w(GoalPost)
     end
 end
