ad7d91e84cada97e0c2563ffd777baae51dacc4a
ali hassan <alithephantom@gmail.com>
2015-12-15 17:45:18 +0200

    make the suthentication return the object

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,9 +2,9 @@ class Api::SessionsController < Api::BaseController
 
   def create
       @data = JSON.parse(request.body.read)
-      user = User.from_facebook_token(@data['token'])
+      @user = User.from_facebook_token(@data['token'])
 
-      if(user)
+      if(@user)
         render json: user.id
       end
   end
---
+++ app/views/api/sessions/create.json.jbuilder
@@ -0,0 +1,9 @@
+json.user do
+	json.first_name @user.first_name
+	json.last_name @user.last_name
+	json.city @user.city
+	json.country @user.country
+	json.gender @user.gender
+	json.date_of_birth @user.date_of_birth
+	json.profile_picture @user.profile_picture
+end
