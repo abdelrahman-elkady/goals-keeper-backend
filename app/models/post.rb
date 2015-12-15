7e4fcf31a86e2aad0acc7cbc7f63acdcb66c38e4
Abkreno <mohamedelhamamsy789@gmail.com>
2015-12-15 16:47:13 +0200

    Mofifies schema and seeds

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 
 	validates :goal, :presence => true
-	validates :creator, :presence => true
+	validates :user, :presence => true
 	validates_length_of :text, :minimum => 5, :maximum => 50, :allow_blank => false
 
 
