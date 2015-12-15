2d0d56b22c8d452e17d4ab127448bb790e399436
Abkreno <mohamedelhamamsy789@gmail.com>
2015-12-15 16:30:17 +0200

    renames post creator to post user

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ActiveRecord::Base
     has_many :comments
     has_many :likes
 
-    belongs_to :creator, :class_name => "User", foreign_key: "creator_id"
+    belongs_to :user
 		belongs_to :goal
 
 end
