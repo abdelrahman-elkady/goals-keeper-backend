48f7792a928f0372219db164a4ff3725e7a2f718
ali hassan <alithephantom@gmail.com>
2015-12-15 12:35:13 +0200

    added show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,28 @@ class UsersController < ApplicationController
   def index
   	@users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+protected
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+  
 end
--- app/views/users/index.json.jbuilder
+++ app/views/users/index.json.jbuilder
@@ -1,4 +1,3 @@
-
 json.users @users do |user|
 	json.first_name user.first_name
 	json.last_name user.last_name
